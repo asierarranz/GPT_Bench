9ad172540dc0c256fde96608f65a60524159d7ee
Asier Arranz <asierarranz@gmail.com>
2019-08-02 09:13:53 -0400

    Initial commit

---
+++ bench345M.py
@@ -0,0 +1,19 @@
+import warnings,time
+warnings.simplefilter(action='ignore', category=FutureWarning)
+
+import time
+import shutil
+import gpt_2_simple as gpt2
+shutil.rmtree('./checkpoint')
+
+sess = gpt2.start_tf_sess()
+t1=time.time()
+print("GPT BENCHMARK STARTED!")
+gpt2.finetune(sess,
+              'shakespeare.txt',
+              model_name="345M",
+              steps=1000) 
+gpt2.generate(sess)
+print("GPT BENCHMARK ENDED!")  
+t2=time.time()-t1
+print("BENCHMARK TIME: " + str(t2) + " seconds")
---
+++ download345M.py
@@ -0,0 +1,5 @@
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
+
+import gpt_2_simple as gpt2
+gpt2.download_gpt2(model_name="345M")
